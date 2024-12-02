90e4a3206d1a86c9ecf2e6b3e76dc840f322c584
Stefan Kip <stefan.kip@wastevision.com>
2024-12-02 11:20:09 +0100

    AoC 2024 days 1 & 2

---
+++ AOC2024.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+var input = (await File.ReadAllLinesAsync("input.txt")).ToList();
+
+var leftNumbers = new List<int>();
+var rightNumbers = new List<int>();
+
+var regex = new Regex(@"(?<digit1>\d+)\s+(?<digit2>\d+)", RegexOptions.Compiled);
+
+foreach (var line in input)
+{
+    var match = regex.Match(line);
+    leftNumbers.Add(int.Parse(match.Groups["digit1"].Value));
+    rightNumbers.Add(int.Parse(match.Groups["digit2"].Value));
+}
+
+Part1(leftNumbers.ToList(), rightNumbers.ToList());
+Part2(leftNumbers.ToList(), rightNumbers.ToList());
+
+void Part1(List<int> leftNumbers, List<int> rightNumbers)
+{
+    leftNumbers = leftNumbers.OrderBy(x => x).ToList();
+    rightNumbers = rightNumbers.OrderBy(x => x).ToList();
+
+    var total = 0;
+    for (var i = 0; i < leftNumbers.Count; i++)
+    {
+        var left = leftNumbers[i];
+        var right = rightNumbers[i];
+        var biggest = Math.Max(left, right);
+        var smallest = Math.Min(left, right);
+        var diff = biggest - smallest;
+        total += diff;
+    }
+
+    Console.WriteLine("Part 1 total: {0}", total);
+}
+
+void Part2(List<int> leftNumbers, List<int> rightNumbers)
+{
+    var total = 0;
+    foreach (var leftNumber in leftNumbers)
+    {
+        var occurrences = rightNumbers.Count(x => x == leftNumber);
+        var multiplied = leftNumber * occurrences;
+        total += multiplied;
+    }
+
+    Console.WriteLine("Part 2 total: {0}", total);
+}
---
+++ AOC2024.2/Program.cs
@@ -0,0 +1,94 @@
+﻿var input = (await File.ReadAllLinesAsync("input.txt")).ToList();
+
+var reports = new List<List<int>>();
+foreach (var line in input)
+{
+    reports.Add(line.Split(' ').Select(int.Parse).ToList());
+}
+
+Part1(reports);
+Console.WriteLine();
+Part2(reports);
+
+void Part1(List<List<int>> reports)
+{
+    var total = 0;
+
+    foreach (var report in reports)
+    {
+        if (IsSafe(report))
+        {
+            total++;
+        }
+    }
+
+    Console.WriteLine("Part 1 total: {0}", total);
+}
+
+void Part2(List<List<int>> reports)
+{
+    var total = 0;
+
+    foreach (var report in reports)
+    {
+        if (IsSafe(report))
+        {
+            total++;
+        }
+        else
+        {
+            for (var i = 0; i < report.Count; i++)
+            {
+                var levels = report.Where((_, index) => index != i).ToList();
+                if (IsSafe(levels))
+                {
+                    total++;
+                    break;
+                }
+            }
+        }
+    }
+
+    Console.WriteLine("Part 2 total: {0}", total);
+}
+
+bool IsSafe(List<int> levels)
+{
+    var isIncreasing = true;
+    var isDecreasing = true;
+
+    for (var i = 1; i < levels.Count; i++)
+    {
+        if (levels[i] > levels[i - 1])
+        {
+            isDecreasing = false;
+        }
+        else if (levels[i] < levels[i - 1])
+        {
+            isIncreasing = false;
+        }
+    }
+
+    if (!isIncreasing && !isDecreasing)
+    {
+        return false;
+    }
+
+    var isSafe = true;
+    for (var i = 1; i < levels.Count; i++)
+    {
+        var num1 = levels[i - 1];
+        var num2 = levels[i];
+        var biggest = Math.Max(num1, num2);
+        var smallest = Math.Min(num1, num2);
+        var diff = biggest - smallest;
+
+        if (diff is < 1 or > 3)
+        {
+            isSafe = false;
+            break;
+        }
+    }
+
+    return isSafe;
+}
