0bf71368c48f35a107e4e0e0c406b259f2acba97
Stefan Kip <stefan.kip@wastevision.com>
2024-12-14 16:45:35 +0100

    AoC 2024 days 13 & 14

---
+++ AOC2024.13/Program.cs
@@ -0,0 +1,120 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static Regex _buttonRegex = new(@"Button .: X\+(?<X>\d+), Y\+(?<Y>\d+)", RegexOptions.Compiled);
+    private static Regex _prizeRegex = new(@"Prize: X=(?<X>\d+), Y=(?<Y>\d+)", RegexOptions.Compiled);
+
+    private static async Task Main(string[] args)
+    {
+        var input = await File.ReadAllLinesAsync("input.txt");
+
+        var machines = input
+            .Chunk(4)
+            .Select(x =>
+                x.Take(3).ToList()
+            )
+            .Select(x => new Machine(x))
+            .ToList();
+
+        Part1(machines);
+        Console.WriteLine();
+        Part2(machines);
+    }
+
+
+    static void Part1(List<Machine> machines)
+    {
+        var total = 0L;
+        foreach (var machine in machines)
+        {
+            total += machine.CalculateCosts();
+        }
+
+        Console.WriteLine($"Part 1 price: {total}");
+    }
+
+    static void Part2(List<Machine> machines)
+    {
+        var total = 0L;
+        foreach (var machine in machines)
+        {
+            total += machine.CalculateCosts(10000000000000);
+        }
+
+        Console.WriteLine($"Part 2 price: {total}");
+    }
+
+    class Machine
+    {
+        public XY ButtonA { get; set; }
+        public XY ButtonB { get; set; }
+        public XY Prize { get; set; }
+        public long ButtonACosts => 3;
+        public long ButtonBCosts => 1;
+
+        public Machine(IList<string> input)
+        {
+            ButtonA = ParseButton(input[0]);
+            ButtonB = ParseButton(input[1]);
+            Prize = ParsePrize(input[2]);
+        }
+
+        public long CalculateCosts(long? addToXY = null)
+        {
+            if (addToXY.HasValue)
+            {
+                Prize.X += addToXY.Value;
+                Prize.Y += addToXY.Value;
+            }
+
+            var a = (decimal)(Prize.X * ButtonB.Y - Prize.Y * ButtonB.X) / (ButtonA.X * ButtonB.Y - ButtonA.Y * ButtonB.X);
+            var b = (decimal)(ButtonA.X * Prize.Y - ButtonA.Y * Prize.X) / (ButtonA.X * ButtonB.Y - ButtonA.Y * ButtonB.X);
+            if (a.Scale == 0 && b.Scale == 0)
+            {
+                Console.WriteLine(this);
+                return (long)a * ButtonACosts + (long)b * ButtonBCosts;
+            }
+
+            return 0;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"{Prize.X}, {Prize.Y}";
+        }
+
+        private static XY ParseButton(string buttonText)
+        {
+            var match = _buttonRegex.Match(buttonText);
+            return new XY
+            {
+                X = long.Parse(match.Groups["X"].Value),
+                Y = long.Parse(match.Groups["Y"].Value),
+            };
+        }
+
+        private static XY ParsePrize(string prizeText)
+        {
+            var match = _prizeRegex.Match(prizeText);
+            return new XY
+            {
+                X = long.Parse(match.Groups["X"].Value),
+                Y = long.Parse(match.Groups["Y"].Value),
+            };
+        }
+    }
+
+    class XY
+    {
+        public long X { get; set; }
+        public long Y { get; set; }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"{X},{Y}";
+        }
+    }
+}
---
+++ AOC2024.14/Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static Regex _regex = new(@"p=(?<px>-?\d+),(?<py>-?\d+) v=(?<vx>-?\d+),(?<vy>-?\d+)", RegexOptions.Compiled);
+    private static int _fieldWidth = 101;
+    private static int _fieldHeight = 103;
+
+    private static async Task Main(string[] args)
+    {
+        var input = await File.ReadAllLinesAsync("input.txt");
+
+        var robots = input
+            .Select(x => _regex.Match(x))
+            .Select(x => new Robot
+            {
+                PX = int.Parse(x.Groups["px"].Value),
+                PY = int.Parse(x.Groups["py"].Value),
+                VX = int.Parse(x.Groups["vx"].Value),
+                VY = int.Parse(x.Groups["vy"].Value),
+            }).ToList();
+
+        Part1(robots.ToList());
+        Console.WriteLine();
+        Part2(robots.ToList());
+    }
+
+
+    static void Part1(List<Robot> robots)
+    {
+        foreach (var robot in robots)
+        {
+            robot.Move(100);
+        }
+
+        var safetyFactor = GetSafetyFactor(robots);
+
+        foreach (var robot in robots)
+        {
+            robot.Reset();
+        }
+
+        Console.WriteLine($"Part 1 safety factor: {safetyFactor}");
+    }
+
+    static long GetSafetyFactor(List<Robot> robots)
+    {
+        var robotsTopLeft = robots.Where(x => x.CurX < _fieldWidth / 2 && x.CurY < _fieldHeight / 2).ToList();
+        var robotsTopRight = robots.Where(x => x.CurX > _fieldWidth / 2 && x.CurY < _fieldHeight / 2).ToList();
+        var robotsBottomLeft = robots.Where(x => x.CurX < _fieldWidth / 2 && x.CurY > _fieldHeight / 2).ToList();
+        var robotsBottomRight = robots.Where(x => x.CurX > _fieldWidth / 2 && x.CurY > _fieldHeight / 2).ToList();
+        return robotsTopLeft.Count * robotsTopRight.Count * robotsBottomLeft.Count * robotsBottomRight.Count;
+    }
+
+    static void Part2(List<Robot> robots)
+    {
+        for (var i = 1; ; i++)
+        {
+            foreach (var robot in robots)
+            {
+                robot.Move(1);
+            }
+
+            if (robots.GroupBy(x => new { x.CurX, x.CurY }).All(x => x.Count() == 1))
+            {
+                Console.WriteLine($"Part 2 seconds: {i}");
+                Console.WriteLine();
+
+                for (var y = 0; y < _fieldHeight; y++)
+                {
+                    for (var x = 0; x < _fieldWidth; x++)
+                    {
+                        var count = robots.Count(r => r.CurX == x && r.CurY == y);
+                        Console.Write(count > 0 ? count.ToString() : '.');
+                    }
+
+                    Console.WriteLine();
+                }
+
+                break;
+            }
+        }
+    }
+
+    private class Robot
+    {
+        public int PX { get; set; }
+        public int PY { get; set; }
+        public int VX { get; set; }
+        public int VY { get; set; }
+        public int? CurX { get; set; }
+        public int? CurY { get; set; }
+
+        public void Move(int times)
+        {
+            if (!CurX.HasValue)
+            {
+                CurX = PX;
+                CurY = PY;
+            }
+
+            CurX += times * VX;
+            CurY += times * VY;
+
+            CurX %= _fieldWidth;
+            CurY %= _fieldHeight;
+
+            if (CurX < 0)
+            {
+                CurX += _fieldWidth;
+            }
+
+            if (CurY < 0)
+            {
+                CurY += _fieldHeight;
+            }
+        }
+
+        public void Reset()
+        {
+            CurX = PX;
+            CurY = PY;
+        }
+
+        public override string ToString()
+        {
+            return $"{CurX},{CurY}";
+        }
+    }
+}
